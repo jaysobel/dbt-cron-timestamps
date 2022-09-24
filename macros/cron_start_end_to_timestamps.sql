501537a096c542c378f71dd9acf70079d2781905
Jay Sobel <22080862+jaysobel@users.noreply.github.com>
2022-09-23 17:09:02 -0700

    Update cron_start_end_to_timestamps.sql

--- macros/cron_start_end_to_timestamps.sql
+++ macros/cron_start_end_to_timestamps.sql
@@ -20,10 +20,10 @@
   :param start_at_column_name: The name of the column in `cte_name` that contains the generation range start.
   :param start_at_column_name: The name of the column in `cte_name` that contains the generation range end.
   :param day_match_mode: Default 'vixie'. Alternatively `contains`, `union` or `intersect`. This parameter controls how day 
-   matching is done. In some implementations of cron, the presence of an asterisk in either the `day_of_month` or `day_of_week` 
-   positions determines whether day matches are unioned or intersected. In `vixie` mode, only the first character is checked, 
-   see [this write up](https://crontab.guru/cron-bug.html) for details. Passing `contains` will check all positions, 
-   while passing `union` or `intersect` will hard-code the behavior, as some modern implementations use `intersect` by default.
+    matching is performed. The day_of_month and day_of_week parts are either unioned or intersected. In some implementations
+    of cron, this is based on the presence of an * in the first position of each entry (vixie). Others expand this 'first entry'
+    to any position of the entry (contains). See [this write up](https://crontab.guru/cron-bug.html) for details.
+    Other implementations use exclusively intersect or union, which can be coded here.
 
   :return: A SQL select statement of ~250 lines that culminates in a distinct selection of: `cron, cron_range_sk, trigger_at_utc`.
 
